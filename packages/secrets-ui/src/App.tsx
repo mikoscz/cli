69e2d781c316e24622e7f58e17655a423e9df1e0
snacksncode <snacksncode@gmail.com>
2022-11-18 12:42:31 +0100

    feat: header basic layout

--- packages/secrets-ui/src/App.tsx
+++ packages/secrets-ui/src/App.tsx
@@ -1,12 +1,23 @@
 import { Button } from "./components/Button";
+import { Input } from "./components/Input/Input";
 import styles from "./styles/App.module.sass";
 
 function App() {
   return (
-    <>
-      <div className={styles.main}>Testing CSS Modules</div>
-      <Button>Hello</Button>
-    </>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.repo}>
+          <span>selleo/dashboard</span> &gt; dev
+        </div>
+        <Button
+          onClick={() => {
+            alert("add");
+          }}
+        >
+          Add
+        </Button>
+      </div>
+    </div>
   );
 }
 
