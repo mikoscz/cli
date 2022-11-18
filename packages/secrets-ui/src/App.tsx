b8b18003ed03accae1277d343d6977e987a6229b
snacksncode <snacksncode@gmail.com>
2022-11-18 12:54:29 +0100

    feat: basic list view

--- packages/secrets-ui/src/App.tsx
+++ packages/secrets-ui/src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input/Input";
 import styles from "./styles/App.module.sass";
 
+const DEFAULT_DATA = [
+  { isSensitive: true, key: "SOME_KEY", value: "SOME_VALUE", isReadOnly: false },
+  { isSensitive: false, key: "SOME_KEY_1", value: "1298371-aldkhasdf-asklfjh", isReadOnly: false },
+  { isSensitive: true, key: "SOME_KEY_2", value: "Hello there", isReadOnly: true },
+  { isSensitive: false, key: "SOME_KEY_3", value: "Brah brah", isReadOnly: false },
+];
+
 function App() {
+  const [secrets, setSecrets] = useState(DEFAULT_DATA);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -16,6 +25,24 @@ function App() {
         >
           Add
         </Button>
+      </div>
+      <div>
+        {secrets.map(({ key, value, isReadOnly, isSensitive }) => {
+          return (
+            <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+              <Input value={key} />
+              <Input type={isSensitive ? "password" : "text"} value={value} />
+              <div>
+                <input type={"checkbox"} checked={isReadOnly} />
+                Read only?
+              </div>
+              <div>
+                <input type={"checkbox"} checked={isSensitive} />
+                Is Sensitive?
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- packages/secrets-ui/src/components/Input/Input.tsx
+++ packages/secrets-ui/src/components/Input/Input.tsx
@@ -1,26 +1,21 @@
+import { ChangeEvent } from "react";
 
-
-import { ChangeEvent } from 'react';
-
-import style from './Input.module.sass';
+import style from "./Input.module.sass";
 
 export interface InputProps {
   value: string;
-  onChange: (newValue: string) => void;
+  onChange?: (newValue: string) => void;
+  type?: "text" | "password";
 }
 
-export const Input = ({
-  value,
-  onChange
-}: InputProps) => {
+export const Input = ({ value, type = "text", onChange }: InputProps) => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value)
-  }
+    onChange?.(event.target.value);
+  };
 
-  return <div className={style.wrapper}>
-    <input
-      value={value}
-      onChange={handleChange}
-    />
-  </div>
+  return (
+    <div className={style.wrapper}>
+      <input type={type} value={value} onChange={handleChange} />
+    </div>
+  );
 };
